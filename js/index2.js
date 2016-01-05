41f251d593a438306350253005e48dcf03aea26d
Shatilla Prayer <prayers@unlv.nevada.edu>
2016-01-05 11:18:20 -0800

    Added Fire button, code cleanup

--- js/index2.js
+++ js/index2.js
@@ -15,9 +15,9 @@ var launch = function() {
   var scene = new BABYLON.Scene(engine);
   var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, BABYLON.Vector3.Zero(), scene);
   var sun = new BABYLON.DirectionalLight("Dir0", new BABYLON.Vector3(0, -1, 0), scene);
-sun.diffuse = new BABYLON.Color3(1, 1, 1);
-sun.specular = new BABYLON.Color3(1, 1, 1);
-sun.intensity = 0.7;
+  sun.diffuse = new BABYLON.Color3(1, 1, 1);
+  sun.specular = new BABYLON.Color3(1, 1, 1);
+  sun.intensity = 0.7;
 
   camera.setPosition(new BABYLON.Vector3(20, 40, 20));
   camera.attachControl(canvas);
@@ -40,18 +40,18 @@ sun.intensity = 0.7;
   var groundMaterial = new BABYLON.StandardMaterial("mountain", scene);
   groundMaterial.diffuseTexture = new BABYLON.Texture("images/mountain.jpg", scene);
   ground.backFaceCulling = false;
- groundMaterial.diffuseTexture.uScale = 30;
-groundMaterial.diffuseTexture.vScale = 40;
+  groundMaterial.diffuseTexture.uScale = 30;
+  groundMaterial.diffuseTexture.vScale = 40;
   ground.material = groundMaterial;
   ground.position.y = 2.0;
 
 
-// wire material
-var wireMaterial = new BABYLON.StandardMaterial("wires", scene);
-wireMaterial.diffuse = new BABYLON.Color3(1, 1, 1);
-wireMaterial.wire = true;
+  // wire material
+  var wireMaterial = new BABYLON.StandardMaterial("wires", scene);
+  wireMaterial.diffuse = new BABYLON.Color3(1, 1, 1);
+  wireMaterial.wire = true;
 
-// sand on the ground
+  // sand on the ground
   var extraGround = BABYLON.Mesh.CreateGround("extraGround", 1400, 1400, 1, scene, false);
   var extraGroundMaterial = new BABYLON.StandardMaterial("extraGround", scene);
   extraGroundMaterial.diffuseTexture = new BABYLON.Texture("Shaders/Ground/sand.jpg", scene);
@@ -72,30 +72,27 @@ wireMaterial.wire = true;
 
   water.isPickable = false;
   water.material = waterMaterial;
-
-
   // Elevation
   var elevationControl = new mountain.elevateMountain(ground);
-
   // Bloom
   var blurWidth = 2.0;
 
-// high threshold light
-/*  var postProcess0 = new BABYLON.PassPostProcess("Scene copy", 1.0, camera);
-  var postProcess1 = new BABYLON.PostProcess("Down sample", "./postprocesses/downsample", ["screenSize", "highlightThreshold"], null, 0.2, camera, BABYLON.Texture.DEFAULT_SAMPLINGMODE);
-  postProcess1.onApply = function (effect) {
-      effect.setFloat2("screenSize", postProcess1.width, postProcess1.height);
-      effect.setFloat("highlightThreshold", 0.85);
-  };
-  var postProcess2 = new BABYLON.BlurPostProcess("Horizontal blur", new BABYLON.Vector2(1.0, 0), blurWidth, 0.5, camera, BABYLON.Texture.DEFAULT_SAMPLINGMODE);
-  var postProcess3 = new BABYLON.BlurPostProcess("Vertical blur", new BABYLON.Vector2(0, 1.0), blurWidth, 0.5, camera, BABYLON.Texture.DEFAULT_SAMPLINGMODE);
-  var postProcess4 = new BABYLON.PostProcess("Final compose", "./postprocesses/compose", ["sceneIntensity", "glowIntensity", "highlightIntensity"], ["sceneSampler"], 1, camera);
-  postProcess4.onApply = function (effect) {
-      effect.setTextureFromPostProcess("sceneSampler", postProcess0);
-      effect.setFloat("sceneIntensity", 0.8);
-      effect.setFloat("glowIntensity", 0.6);
-      effect.setFloat("highlightIntensity", 1.5);
-  };*/
+  // high threshold light
+  /*  var postProcess0 = new BABYLON.PassPostProcess("Scene copy", 1.0, camera);
+    var postProcess1 = new BABYLON.PostProcess("Down sample", "./postprocesses/downsample", ["screenSize", "highlightThreshold"], null, 0.2, camera, BABYLON.Texture.DEFAULT_SAMPLINGMODE);
+    postProcess1.onApply = function (effect) {
+        effect.setFloat2("screenSize", postProcess1.width, postProcess1.height);
+        effect.setFloat("highlightThreshold", 0.85);
+    };
+    var postProcess2 = new BABYLON.BlurPostProcess("Horizontal blur", new BABYLON.Vector2(1.0, 0), blurWidth, 0.5, camera, BABYLON.Texture.DEFAULT_SAMPLINGMODE);
+    var postProcess3 = new BABYLON.BlurPostProcess("Vertical blur", new BABYLON.Vector2(0, 1.0), blurWidth, 0.5, camera, BABYLON.Texture.DEFAULT_SAMPLINGMODE);
+    var postProcess4 = new BABYLON.PostProcess("Final compose", "./postprocesses/compose", ["sceneIntensity", "glowIntensity", "highlightIntensity"], ["sceneSampler"], 1, camera);
+    postProcess4.onApply = function (effect) {
+        effect.setTextureFromPostProcess("sceneSampler", postProcess0);
+        effect.setFloat("sceneIntensity", 0.8);
+        effect.setFloat("glowIntensity", 0.6);
+        effect.setFloat("highlightIntensity", 1.5);
+    };*/
 
   // Render loop
   var renderFunction = function() {
@@ -125,10 +122,6 @@ wireMaterial.wire = true;
     skybox.rotation.y += 0.0001 * scene.getAnimationRatio();
   };
 
-// mountains
-// console.log("positions", elevationControl.getFacesOfVertices());
-
-
   // Launch render loop
   scene.executeWhenReady(function() {
     engine.runRenderLoop(renderFunction);
@@ -143,14 +136,14 @@ wireMaterial.wire = true;
   // UI
   var cameraButton = document.getElementById("cameraButton");
   var elevationButton = document.getElementById("elevationButton");
-//  var digButton = document.getElementById("digButton");
+  //  var digButton = document.getElementById("digButton");
   var fireButton = document.getElementById("volcanoButton");
 
   window.oncontextmenu = function() {
     return false;
   };
 
-// Camera Mode
+  // Camera Mode
   cameraButton.addEventListener("pointerdown", function() {
     if (mode == "CAMERA")
       return;
@@ -160,10 +153,11 @@ wireMaterial.wire = true;
     mode = "CAMERA";
     cameraButton.className = "buttons selected";
     // digButton.className = "buttons";
+    fireButton.className = "buttons";
     elevationButton.className = "buttons";
   });
 
-// Mountain Mode
+  // Mountain Mode
   elevationButton.addEventListener("pointerdown", function() {
     if (mode == "ELEVATION")
       return;
@@ -178,75 +172,60 @@ wireMaterial.wire = true;
 
     elevationButton.className = "buttons selected";
     cameraButton.className = "buttons";
+    fireButton.className = "buttons";
   });
 
-  //When click event is raised
-  /*window.addEventListener("click", function() {
-    var pickResult = scene.pick(scene.pointerX, scene.pointerY);
+  elevationButton.addEventListener("pointerdown", function() {
+    if (mode == "FIRE"){
+    /*  if (pickResult.hit) {
+        ABox.position.x = pickResult.pickedPoint.x;
+        ABox.position.y = pickResult.pickedPoint.y;
+      }*/
+      return;
+    }
 
 
-    if (clicked == true) {
-      var xVal = pickResult.pickedPoint.x;
-      var yVal = pickResult.pickedPoint.y;
-      var zVal = pickResult.pickedPoint.z;
-      if (pickResult.hit) {*/
-        var ABox = BABYLON.Mesh.CreateBox("theBox2", 1.0, scene, true, BABYLON.Mesh.DEFAULTSIDE);
-        ABox.position = new BABYLON.Vector3(-225, 10, -225);
-        // Fire Particle System
-        // particle emitter
-        //  var fountain = BABYLON.Mesh.CreateBox("fountain", 20.0, scene);
-        // Create a particle system
-        var particleSystem = new BABYLON.ParticleSystem("particles", 2000, scene);
+    if (mode == "CAMERA") {
+      camera.detachControl(canvas);
+      elevationControl.attachControl(canvas);
+    }
 
-        //Texture of each particle
-        particleSystem.particleTexture = new BABYLON.Texture("textures/Flare.png", scene);
+    mode = "FIRE";
 
-        // Where the particles come from
-        //  particleSystem.emitter = fountain; // the starting object, the emitter
-        particleSystem.emitter = ABox;
-        particleSystem.minEmitBox = new BABYLON.Vector3(-1, 0, 0); // Starting all from
-        particleSystem.maxEmitBox = new BABYLON.Vector3(1, 0, 0); // To...
+    elevationButton.className = "buttons";
+    cameraButton.className = "buttons";
+    fireButton.className = "buttons selected";
+  });
 
-        // Colors of all particles
-        particleSystem.color1 = new BABYLON.Color4(0.8, 0.1, 0, 1.0);
-        particleSystem.color2 = new BABYLON.Color4(1, 0, 0, 1.0);
-        particleSystem.colorDead = new BABYLON.Color4(0, 0, 0, 0.0);
+  // Fire Particle System
+/*  var ABox = BABYLON.Mesh.CreateBox("theBox2", 1.0, scene, true, BABYLON.Mesh.DEFAULTSIDE);
+  var particleSystem = new BABYLON.ParticleSystem("particles", 2000, scene);
+  particleSystem.particleTexture = new BABYLON.Texture("textures/Flare.png", scene);
+  particleSystem.emitter = ABox;
+  particleSystem.minEmitBox = new BABYLON.Vector3(-1, 0, 0); // Starting all from
+  particleSystem.maxEmitBox = new BABYLON.Vector3(1, 0, 0); // To...
 
-        // Size of each particle (random between...
-        particleSystem.minSize = 5;
-        particleSystem.maxSize = 10;
+  particleSystem.color1 = new BABYLON.Color4(0.8, 0.1, 0, 1.0);
+  particleSystem.color2 = new BABYLON.Color4(1, 0, 0, 1.0);
+  particleSystem.colorDead = new BABYLON.Color4(0, 0, 0, 0.0);
 
-        // Life time of each particle (random between...
-        particleSystem.minLifeTime = 0.3;
-        particleSystem.maxLifeTime = 1.5;
+  particleSystem.minSize = 5;
+  particleSystem.maxSize = 10;
+  particleSystem.minLifeTime = 0.3;
+  particleSystem.maxLifeTime = 1.5;
+  particleSystem.emitRate = 2500;
 
-        // Emission rate
-        particleSystem.emitRate = 2500;
+  particleSystem.blendMode = BABYLON.ParticleSystem.BLENDMODE_ONEONE;
+  particleSystem.gravity = new BABYLON.Vector3(0, 35, 0);
+  particleSystem.direction1 = new BABYLON.Vector3(-7, 8, 3);
+  particleSystem.direction2 = new BABYLON.Vector3(7, 8, -3);
 
-        // Blend mode : BLENDMODE_ONEONE, or BLENDMODE_STANDARD
-        particleSystem.blendMode = BABYLON.ParticleSystem.BLENDMODE_ONEONE;
+  particleSystem.minAngularSpeed = 0;
+  particleSystem.maxAngularSpeed = Math.PI;
+  particleSystem.minEmitPower = 1;
+  particleSystem.maxEmitPower = 3;
+  particleSystem.updateSpeed = 0.005;
 
-        // Set the gravity of all particles
-        particleSystem.gravity = new BABYLON.Vector3(0, 35, 0);
-
-        // Direction of each particle after it has been emitted
-        particleSystem.direction1 = new BABYLON.Vector3(-7, 8, 3);
-        particleSystem.direction2 = new BABYLON.Vector3(7, 8, -3);
-
-        // Angular speed, in radians
-        particleSystem.minAngularSpeed = 0;
-        particleSystem.maxAngularSpeed = Math.PI;
-
-        // Speed
-        particleSystem.minEmitPower = 1;
-        particleSystem.maxEmitPower = 3;
-        particleSystem.updateSpeed = 0.005;
-
-        // Start the particle system
-        particleSystem.start();
-  //    }
-//    }
-//  });
-
+  particleSystem.start();*/
 
 };
